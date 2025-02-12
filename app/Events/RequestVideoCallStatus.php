3aa86d3f110ad0767e0da146b5a118ce4a5eb640
paullarasales <rkise2131@gmail.com>
2025-02-12 15:29:06 +0800

    done modifying channels

--- app/Events/RequestVideoCallStatus.php
+++ app/Events/RequestVideoCallStatus.php
@@ -2,11 +2,13 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -17,7 +19,7 @@ class RequestVideoCallStatus
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public User $user)
     {
         //
     }
@@ -30,7 +32,7 @@ class RequestVideoCallStatus
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new PrivateChannel("video-call.{$this->user->id}"),
         ];
     }
 }
