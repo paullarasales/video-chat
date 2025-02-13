c5b7d37f43263868513a078374d10bfaa2649c5c
paullarasales <rkise2131@gmail.com>
2025-02-13 22:54:05 +0800

    done defining the relationship

--- app/Models/VideoCallParticipant.php
+++ app/Models/VideoCallParticipant.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VideoCallParticipant extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['video_call_id', 'user_id'];
+
+    public function videoCall()
+    {
+        return $this->belongsTo(VideoCall::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
